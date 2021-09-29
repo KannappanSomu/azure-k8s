56a3037c621836e58a27ab7428674e822d0cf8c1
ksenthil <ksenthil@volvocars.com>
2021-09-29 23:14:40 +0200

    iac

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -7,7 +7,7 @@
 data azurerm_client_config current {}
 
 resource azurerm_key_vault key_vault {
-  name                = "${var.vault_name}"
+  name                = var.vault_name
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
